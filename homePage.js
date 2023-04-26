3565a4e07887e8c223e6526a721bcc2baefbcbba
MagnusTro <magnustro@gmail.com>
2023-04-26 15:27:08 +0200

    Moved js to js
    moved some js code to js file from html file

--- homePage.js
+++ homePage.js
@@ -16,3 +16,82 @@ images.forEach(image => {
     img.class = "column";
     document.getElementById("imageList").appendChild(img);
 });
+
+function toggleEditor() {
+    var element = document.getElementById("editorContainer");
+    if (element.style.display === "none") {
+      element.style.display = "block";
+      editor.refresh();
+    } else {
+      element.style.display = "none";
+    }
+  }
+
+  function toggleHtml() {
+    var htmlEditorWindow = document.getElementById("htmlEditorDiv");
+    var cssEditorWindow = document.getElementById("cssEditorDiv");
+    var jsEditorWindow = document.getElementById("jsEditorDiv");
+
+    var htmlButton = document.getElementById("html");
+    var cssButton = document.getElementById("css");
+    var jsButton = document.getElementById("js");
+
+    htmlButton.style.color = "#fe5000";
+    htmlButton.style.backgroundColor = "#000000";
+    cssButton.style.color = "#000000";
+    cssButton.style.backgroundColor = "#ffffff";
+    jsButton.style.color = "#000000";
+    jsButton.style.backgroundColor = "#ffffff";
+
+    cssEditorWindow.style.display = "none";
+    jsEditorWindow.style.display = "none";
+    htmlEditorWindow.style.display = "block";
+
+    editor.refresh();
+  }
+
+  function toggleCss() {
+    var htmlEditorWindow = document.getElementById("htmlEditorDiv");
+    var cssEditorWindow = document.getElementById("cssEditorDiv");
+    var jsEditorWindow = document.getElementById("jsEditorDiv");
+
+    var htmlButton = document.getElementById("html");
+    var cssButton = document.getElementById("css");
+    var jsButton = document.getElementById("js");
+
+    cssButton.style.color = "#fe5000";
+    cssButton.style.backgroundColor = "#000000";
+    htmlButton.style.color = "#000000";
+    htmlButton.style.backgroundColor = "#ffffff";
+    jsButton.style.color = "#000000";
+    jsButton.style.backgroundColor = "#ffffff";
+
+    htmlEditorWindow.style.display = "none";
+    cssEditorWindow.style.display = "block";
+    jsEditorWindow.style.display = "none";
+
+    editor.refresh();
+  }
+
+  function toggleJs() {
+    var htmlEditorWindow = document.getElementById("htmlEditorDiv");
+    var cssEditorWindow = document.getElementById("cssEditorDiv");
+    var jsEditorWindow = document.getElementById("jsEditorDiv");
+
+    var htmlButton = document.getElementById("html");
+    var cssButton = document.getElementById("css");
+    var jsButton = document.getElementById("js");
+
+    jsButton.style.color = "#fe5000";
+    jsButton.style.backgroundColor = "#000000";
+    cssButton.style.color = "#000000";
+    cssButton.style.backgroundColor = "#ffffff";
+    htmlButton.style.color = "#000000";
+    htmlButton.style.backgroundColor = "#ffffff";
+    
+    htmlEditorWindow.style.display = "none";
+    cssEditorWindow.style.display = "none";
+    jsEditorWindow.style.display = "block";
+
+    editor.refresh();
+  }
