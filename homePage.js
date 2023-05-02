261a95d032e3e4fa33f70925b109d58df5ca7573
joel90688 <ni5@kth.se>
2023-05-02 18:05:01 +0200

    ADD kod i ruta, ADD collapsable, FIX städat

--- homePage.js
+++ homePage.js
@@ -1,4 +1,5 @@
-var images = [{number: 1, link: "images/bg9.png"}, 
+var images = [
+{number: 1, link: "images/bg9.png"}, 
 {number: 2, link: "images/bg1.png"},
 {number: 3, link: "images/bg2.png"}, 
 {number: 4, link: "images/bg3.png"},
@@ -16,3 +17,88 @@ images.forEach(image => {
     img.class = "column";
     document.getElementById("imageList").appendChild(img);
 });
+
+// initialize CodeMirror editor
+var jsEditor = CodeMirror.fromTextArea(document.getElementById("jscode"), {
+  lineNumbers: true,
+  mode: "javascript",
+});
+
+var htmlEditor = CodeMirror.fromTextArea(document.getElementById("htmlcode"), {
+  lineNumbers: true,
+  mode: "html"
+});
+
+htmlEditor.setSize("100%", "100%");
+
+var cssEditor = CodeMirror.fromTextArea(document.getElementById("csscode"), {
+  lineNumbers: true,
+  mode: "css"
+});
+
+//kod från video med collapsable grej
+const hamburger = document.getElementById('hamburger')
+const sidebar = document.getElementById('sidebar')
+const overlay = document.getElementById('overlay')
+
+let menuOpen = false
+
+function openMenu() {
+  menuOpen = true
+  sidebar.style.display = 'block'
+  overlay.style.display = 'block'
+  /*sidebar.style.width = '50vw'*/
+}
+
+function closeMenu() {
+  menuOpen = false
+  overlay.style.display = 'none'
+  sidebar.style.display = 'block'
+  /*sidebar.style.width = '0px'*/
+}
+
+hamburger.addEventListener('click', function () {
+  if (!menuOpen) {
+    openMenu()
+  }
+})
+
+overlay.addEventListener('click', function () {
+  if (menuOpen) {
+    closeMenu()
+  }
+})
+
+
+
+//martinkod
+htmlEditor.getDoc().setValue(document.documentElement.outerHTML);
+
+document.getElementById('htmlButton').addEventListener('click', () => {
+  htmlEditor.getDoc().setValue(document.documentElement.outerHTML);
+  });
+
+document.getElementById('saveButton').addEventListener('click', updatePageFromFullHtml);
+
+function updatePageFromFullHtml() {
+  const newHtml = htmlEditor.getValue();
+
+  // Create a temporary DOM element to parse the full HTML
+  const tempElement = document.createElement('html');
+  tempElement.innerHTML = newHtml;
+
+  // Save the content of the fullHtmlCode textarea
+  const fullHtmlContent = newHtml;
+
+  // Replace the current DOM with the new one
+  document.replaceChild(tempElement, document.documentElement);
+
+  // Restore the content of the fullHtmlCode textarea
+  htmlEditor.getDoc().setValue(fullHtmlContent);
+
+  // Reattach the event listener for the "Save Changes" button
+  document.getElementById('saveButton').addEventListener('click', updatePageFromFullHtml);
+
+  // Trigger the "Refresh Code" button event to update the textarea
+  document.getElementById('htmlButton').click();
+}
