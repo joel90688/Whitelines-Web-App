6023736bbf57caab796ea6a30769447ebbd2c2c8
kthfatalerror <119874018+kthfatalerror@users.noreply.github.com>
2023-05-13 12:54:42 +0200

    fixcommit

--- homePage.js
+++ homePage.js
@@ -113,7 +113,6 @@ overlay.addEventListener('click', function () {
 })
 
 document.getElementById('saveButton').addEventListener('click', () => {
-<<<<<<< HEAD
 
   const code = jsEditor.getValue();
   // save to local storage
@@ -136,39 +135,3 @@ document.getElementById('saveButton').addEventListener('click', () => {
       console.log(localStorage.getItem('savedCode'));
       eval(localStorage.getItem('savedCode'));
     };
-=======
-  eval(jsEditor.getValue());
-});
-
-/* //martinkod
-htmlEditor.getDoc().setValue(document.documentElement.outerHTML);
-
-document.getElementById('htmlButton').addEventListener('click', () => {
-  htmlEditor.getDoc().setValue(document.documentElement.outerHTML);
-  });
-
-document.getElementById('saveButton').addEventListener('click', updatePageFromFullHtml);
-
-function updatePageFromFullHtml() {
-  const newHtml = htmlEditor.getValue();
-
-  // Create a temporary DOM element to parse the full HTML
-  const tempElement = document.createElement('html');
-  tempElement.innerHTML = newHtml;
-
-  // Save the content of the fullHtmlCode textarea
-  const fullHtmlContent = newHtml;
-
-  // Replace the current DOM with the new one
-  document.replaceChild(tempElement, document.documentElement);
-
-  // Restore the content of the fullHtmlCode textarea
-  htmlEditor.getDoc().setValue(fullHtmlContent);
-
-  // Reattach the event listener for the "Save Changes" button
-  document.getElementById('saveButton').addEventListener('click', updatePageFromFullHtml);
-
-  // Trigger the "Refresh Code" button event to update the textarea
-  document.getElementById('htmlButton').click();
-} */
->>>>>>> parent of f39d597 (dfghfdgf)
