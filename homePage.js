19d48b0aeaf550040e3e68824d0ef49f91e3dc44
kthfatalerror <119874018+kthfatalerror@users.noreply.github.com>
2023-05-13 12:45:02 +0200

    fixcommit

--- homePage.js
+++ homePage.js
@@ -1,5 +1,28 @@
+var githubButton = document.getElementById('githubButton');
+var githubButtonExtra = document.getElementById('githubButtonExtra');
+
+githubButton.addEventListener('mouseover', function () {
+  githubButtonExtra.style.opacity = '1';
+  githubButton.style.marginRight = '5vw';
+});
+
+githubButtonExtra.addEventListener('mouseover', function () {
+  githubButtonExtra.style.opacity = '1';
+  githubButton.style.marginRight = '5vw';
+});
+
+githubButtonExtra.addEventListener('mouseout', function () {
+  githubButtonExtra.style.opacity = '0';
+  githubButton.style.marginRight = '0vw';
+});
+
+githubButton.addEventListener('mouseout', function () {
+  githubButtonExtra.style.opacity = '0';
+  githubButton.style.marginRight = '0vw';
+});
+
 const fileInput = document.getElementById("file-input");
-//  localStorage.setItem('savedCode','');
+
 
 var images = [
 {number: 1, link: "images/bg9.png"}, 
@@ -13,45 +36,45 @@ var images = [
 {number: 9, link: "images/bg8.png"}];
 
 images.forEach(image => {
-const img = document.createElement("img");
-img.style.cssText = 'float: left;width: 25%;padding: 5px;cursor: pointer';
-img.src = image.link;
-img.id = "image" + image.number;
-img.class = "column";
-img.addEventListener('mouseover',
-  function(){ img.style.outline = "2px dotted grey";});
-img.addEventListener('mouseout',
-  function(){ img.style.outline = "none";});
-document.getElementById("imageList").appendChild(img);
+  const img = document.createElement("img");
+  img.style.cssText = 'float: left;width: 25%;padding: 5px;cursor: pointer';
+  img.src = image.link;
+  img.id = "image" + image.number;
+  img.class = "column";
+  img.addEventListener('mouseover',
+    function(){ img.style.outline = "2px dotted grey";});
+  img.addEventListener('mouseout',
+    function(){ img.style.outline = "none";});
+  document.getElementById("imageList").appendChild(img);
 
-img.addEventListener('click', function () {
-  window.location.href = "edit.html";
-  localStorage.setItem("picture", img.src);
-})
+  img.addEventListener('click', function () {
+    window.location.href = "edit.html";
+    localStorage.setItem("picture", img.src);
+  })
 });
 
 
 fileInput.addEventListener('change', function (){
-let file = fileInput.files[0];
-  if(!file) return;
-console.log(file);
-images.push({})
-  //previewImg.src = URL.createObjectURL(file);
-  //previewImg.addEventListener("load", () => {
-  //    resetFilterBtn.click();
-  //    document.querySelector(".container").classList.remove("disable");
-  //});
+  let file = fileInput.files[0];
+    if(!file) return;
+  console.log(file);
+  images.push({})
+    //previewImg.src = URL.createObjectURL(file);
+    //previewImg.addEventListener("load", () => {
+    //    resetFilterBtn.click();
+    //    document.querySelector(".container").classList.remove("disable");
+    //});
 });
 
 // initialize CodeMirror editor
 /* var jsEditor = CodeMirror.fromTextArea(document.getElementById("jscode"), {
-lineNumbers: true,
-mode: "javascript",
+  lineNumbers: true,
+  mode: "javascript",
 }); */
 
 var jsEditor = CodeMirror.fromTextArea(document.getElementById("jscode"), {
-lineNumbers: true,
-mode: "html"
+  lineNumbers: true,
+  mode: "html"
 });
 
 jsEditor.setSize("100%", "100%");
@@ -65,31 +88,32 @@ const overlay = document.getElementById('overlay')
 let menuOpen = false
 
 function openMenu() {
-menuOpen = true
-overlay.style.display = 'block'
-sidebar.style.width = '50vw'
+  menuOpen = true
+  overlay.style.display = 'block'
+  sidebar.style.width = '50vw'
 }
 
 function closeMenu() {
-menuOpen = false
-overlay.style.display = 'none'
-sidebar.style.display = 'block'
-sidebar.style.width = '0px'
+  menuOpen = false
+  overlay.style.display = 'none'
+  sidebar.style.display = 'block'
+  sidebar.style.width = '0px'
 }
 
 hamburger.addEventListener('click', function () {
-if (!menuOpen) {
-  openMenu()
-}
+  if (!menuOpen) {
+    openMenu()
+  }
 })
 
 overlay.addEventListener('click', function () {
-if (menuOpen) {
-  closeMenu()
-}
+  if (menuOpen) {
+    closeMenu()
+  }
 })
 
 document.getElementById('saveButton').addEventListener('click', () => {
+<<<<<<< HEAD
 
   const code = jsEditor.getValue();
   // save to local storage
@@ -112,3 +136,39 @@ document.getElementById('saveButton').addEventListener('click', () => {
       console.log(localStorage.getItem('savedCode'));
       eval(localStorage.getItem('savedCode'));
     };
+=======
+  eval(jsEditor.getValue());
+});
+
+/* //martinkod
+htmlEditor.getDoc().setValue(document.documentElement.outerHTML);
+
+document.getElementById('htmlButton').addEventListener('click', () => {
+  htmlEditor.getDoc().setValue(document.documentElement.outerHTML);
+  });
+
+document.getElementById('saveButton').addEventListener('click', updatePageFromFullHtml);
+
+function updatePageFromFullHtml() {
+  const newHtml = htmlEditor.getValue();
+
+  // Create a temporary DOM element to parse the full HTML
+  const tempElement = document.createElement('html');
+  tempElement.innerHTML = newHtml;
+
+  // Save the content of the fullHtmlCode textarea
+  const fullHtmlContent = newHtml;
+
+  // Replace the current DOM with the new one
+  document.replaceChild(tempElement, document.documentElement);
+
+  // Restore the content of the fullHtmlCode textarea
+  htmlEditor.getDoc().setValue(fullHtmlContent);
+
+  // Reattach the event listener for the "Save Changes" button
+  document.getElementById('saveButton').addEventListener('click', updatePageFromFullHtml);
+
+  // Trigger the "Refresh Code" button event to update the textarea
+  document.getElementById('htmlButton').click();
+} */
+>>>>>>> parent of f39d597 (dfghfdgf)
